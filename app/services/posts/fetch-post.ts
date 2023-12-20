567fdf5328e4b39cc19ee89b4ac32282aa34a65f
Sima Amini <sima.amini@connect2wow.com>
2023-12-20 20:58:54 +0330

    Add revalidation time

--- app/services/posts/fetch-post.ts
+++ app/services/posts/fetch-post.ts
@@ -1,5 +1,7 @@
 export const fetchPost = async (id: number) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: { revalidate: 3600 },
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
--- app/services/posts/fetch-posts.ts
+++ app/services/posts/fetch-posts.ts
@@ -1,5 +1,7 @@
 export const fetchPosts = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: { revalidate: 3600 },
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
